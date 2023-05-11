5f6e3f426292989f09eea8f209a113764658f666
dvny <danaildimitrov2003@gmail.com>
2023-05-11 15:07:01 +0300

    Initial Commit

---
+++ AutoVitesse/AutoVitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoVitesseApp.swift
+//  AutoVitesse
+//
+//  Created by Danail Dimitrov on 11.05.23.
+//
+
+import SwiftUI
+
+@main
+struct AutoVitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
